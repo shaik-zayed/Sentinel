402599b6f0d6ed3e4b8cf0e1f98f9c003a3ec725
shaik-zayed <smdzayed@gmail.com>
2026-04-02 17:52:07 +0530

    Woking on docker locally, There are still issues that need to be fixed, mainly regarding the report downloading part from minio

--- report-service/src/main/java/org/sentinel/reportservice/service/MinioStorageService.java
+++ report-service/src/main/java/org/sentinel/reportservice/service/MinioStorageService.java
@@ -4,7 +4,6 @@ import io.minio.*;
 import io.minio.http.Method;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
